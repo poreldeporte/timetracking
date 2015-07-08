fecedfca45a2c92e78bbb226f6c65e9b01f15fd0
Franco Viola <franco.viola@live.com>
2015-07-08 14:43:23 -0400

    Nizar wants us to do something weird. This is before

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,4 +5,13 @@ class EntriesController < ApplicationController
 		@hours = @project.total_hours_in_month(now.month, now.year)
 		render('index')
 	end 
+	def new
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.new
+
+		render('new')
+	end 
+	def create
+		
+	end 
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,10 +2,10 @@ class Entry < ActiveRecord::Base
 	belongs_to :project
 
 	validates :hours, numericality: true
-	validates :minutes, numericality: true
-	validates :date, presence: true
 	validates :hours, presence: true
+	validates :minutes, numericality: true
 	validates :minutes, presence: true
+	validates :date, presence: true
 	validates :project_id, presence: true
 
 end
