b822f570701d3b4ad38818229d8a146ef75a424d
Franco Viola <franco.viola@live.com>
2015-07-08 15:00:06 -0400

    added the resource code to routes to replace all the useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,19 @@ Rails.application.routes.draw do
 
   get '/contact' => 'site#contact'
 
-  get '/projects' => 'projects#index'
+  resources :projects do
+      resources :entries
+  end 
+  
+  # get '/projects' => 'projects#index'
 
-  get '/projects/:id' => 'projects#show'
+  # get '/projects/:id' => 'projects#show'
 
-  get '/projects/:project_id/entries' => 'entries#index'
+  # get '/projects/:project_id/entries' => 'entries#index'
 
-  get '/projects/:project_id/entries/new' => 'entries#new'
+  # get '/projects/:project_id/entries/new' => 'entries#new'
 
-  post '/projects/:project_id/entries' => 'entries#create', :as => 'project_entries'
+  # post '/projects/:project_id/entries' => 'entries#create', :as => 'project_entries'
 
  
   # The priority is based upon order of creation: first created -> highest priority.
