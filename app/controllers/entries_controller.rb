7bb46ddeb7480ca1cfa28bbb0633f9726f4148ec
Franco Viola <franco.viola@live.com>
2015-07-08 11:30:14 -0400

    Added a total_hours_in_month function to the Project Class, and called on Entries controller to sum up hours for every entry in a given project

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,8 @@
 class EntriesController < ApplicationController
 	def index
+		now = DateTime.now
 		@project = Project.find(params[:project_id])
-
+		@hours = @project.total_hours_in_month(now.month, now.year)
 		render('index')
 	end 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,18 @@ class Project < ActiveRecord::Base
 	def self.last_created(lim)
 		return self.order(:created_at => :desc).limit(lim)
 	end 
+	def total_hours_in_month(month, year)
+		start = DateTime.new(year, month, 1)
+		cutoff = DateTime.new(year, month, -1)
+		# date = start...cutoff
+		range = self.entries.where("date > ?", start).where("date < ?", cutoff)
+		hours = range.reduce(0) do |sum, rng|
+			sum + rng.hours
+		end 
+		minutes = range.reduce(0) do |sum, rng|
+			sum + rng.minutes
+		end 
+		total = hours + (minutes / 60.0)
+		return total
+	end 
 end
