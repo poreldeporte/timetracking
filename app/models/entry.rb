0155b74e70849d1899d19519672829f987fc2a78
Franco Viola <franco.viola@live.com>
2015-07-08 12:11:54 -0400

    Added validation to Entry and Project for its attributes

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,11 @@
 class Entry < ActiveRecord::Base
 	belongs_to :project
+
+	validates :hours, numericality: true
+	validates :minutes, numericality: true
+	validates :date, presence: true
+	validates :hours, presence: true
+	validates :minutes, presence: true
+	validates :project_id, presence: true
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,24 @@
 class Project < ActiveRecord::Base
 	has_many :entries
+
+	validates :name, uniqueness: true
+	validates :name, presence: true
+	validates :name, length: {maximum: 30}
+
 	def self.clean_new
 		return self.where("created_at > ?", Date.current - 1.week).destroy_all
 	end 
 	def self.last_created(lim)
 		return self.order(:created_at => :desc).limit(lim)
 	end 
-	def total_hours_in_month(month, year)
+	def model_entries(month, year)
 		start = DateTime.new(year, month, 1)
 		cutoff = DateTime.new(year, month, -1)
-		# date = start...cutoff
-		range = self.entries.where("date > ?", start).where("date < ?", cutoff)
+		return self.entries.where("date > ?", start).where("date < ?", cutoff)
+
+	end 
+	def total_hours_in_month(month, year)
+		range = self.model_entries(month, year)
 		hours = range.reduce(0) do |sum, rng|
 			sum + rng.hours
 		end 
