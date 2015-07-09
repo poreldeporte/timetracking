4a4ce9f89f8ed1230b8a598487c3a45c31a80315
Franco Viola <franco.viola@live.com>
2015-07-09 10:23:38 -0400

    Added Delete function to each entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -20,6 +20,33 @@ class EntriesController < ApplicationController
 			render('new')
 		end
 	end 
+	def edit
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.find(params[:id])
+
+		render('edit')
+	end 
+	def update
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.find(params[:id])
+
+		if @entry.update_attributes(entry_params)
+			redirect_to(project_entries_path)
+		else
+			render('edit')
+		end
+	end 
+	def destroy
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.find(params[:id])
+
+		if @entry.destroy
+			redirect_to(project_entries_path)
+		else
+			render('index')
+		end
+
+	end
 	private 
 	def entry_params
 		return params.require(:entry).permit(:hours, :minutes, :comment, :date)
